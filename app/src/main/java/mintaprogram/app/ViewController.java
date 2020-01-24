6d35c3fa18bbf05f670320e3e7f79245511ebda8
PC <PC@192.168.1.57>
2020-01-24 08:07:53 +0100

    init

---
+++ app/src/main/java/mintaprogram/app/ViewController.java
@@ -0,0 +1,30 @@
+package mintaprogram.app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Rédai Dávid
+ */
+public class ViewController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
